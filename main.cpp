7b0520a1c1ed19210c6be1cf9a151c95fd7b0c7b
Smit417263 <smitp8720@gmail.com>
2020-01-24 17:59:49 -0800

    changed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
-
 #include "c-echo.h"
 
 int main(int argv, char** argc) {
-    std::cout << echo(argv, argc) << "\n";
+    std::cout << "Count of output: " << count(echo(argv,argc)) << std::endl;
 }
